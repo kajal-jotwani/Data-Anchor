4d48529f263c9fe216f0675311c773a6abd1cd8b
kajal-jotwani <kajaljotwani06@gmail.com>
2024-08-31 23:00:39 +0530

    updated locationlist

--- frontend/src/components/LocationList.jsx
+++ frontend/src/components/LocationList.jsx
@@ -1,26 +1,27 @@
+
 import React from 'react';
 import './LocationList.css';
 
 const LocationList = ({ locations }) => {
   if (!locations || locations.length === 0) {
-    return <div>No locations available.</div>;
+    return <div>No locations available. Try searching.</div>;
   }
-
-  
 
   return (
     <div className="location-list">
       <h2>Renewable Energy Locations</h2>
       <ul>
-        {locations.map((location) => (
-          <li key={location._id}>
+        {locations.map((location, index) => (
+          <li key={index}>
             <h3>{location.name}</h3>
-            <p>Latitude: {location.latitude}</p>
-            <p>Longitude: {location.longitude}</p>
-            <p>Climate Conditions: {location.climateConditions}</p>
-            <p>Water Supply: {location.waterSupply ? 'Yes' : 'No'}</p>
-            <p>Low Carbon Emission: {location.lowCarbonEmission ? 'Yes' : 'No'}</p>
-            <p>Other Factors: {location.otherFactors}</p>
+            <div className="card">
+              <p><span className="label">Latitude:</span> <span className="value">{location.latitude}</span></p>
+              <p><span className="label">Longitude:</span> <span className="value">{location.longitude}</span></p>
+              <p><span className="label">Climate Conditions:</span> <span className="value">{location.climateConditions}</span></p>
+              <p><span className="label">Water Supply:</span> <span className="value">{location.waterSupply ? 'Yes' : 'No'}</span></p>
+              <p><span className="label">Low Carbon Emission:</span> <span className="value">{location.lowCarbonEmission ? 'Yes' : 'No'}</span></p>
+              <p><span className="label">Other Factors:</span> <span className="value">{location.otherFactors}</span></p>
+            </div>
           </li>
         ))}
       </ul>
