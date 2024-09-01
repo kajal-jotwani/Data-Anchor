2bb654ca22505f6a23be722a77a31ad12d0804ed
kajal-jotwani <kajaljotwani06@gmail.com>
2024-09-01 23:44:29 +0530

    header component

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,10 +4,21 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <h1>Renewable Data Center Locator</h1>
+      <div className="logo">
+        <h1>Data Anchor</h1>
+      </div>
+      <nav className="navbar">
+        <ul className="nav-list">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About Us</a></li>
+          <li><a href="#Increasing Efficiency">Increasing Efficiency</a></li>
+          <li><a href="#Reducing Carbon">Reducing Carbon</a></li>
+          <li><a href="#Locations">Locations</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </nav>
     </header>
   );
 };
 
 export default Header;
-
